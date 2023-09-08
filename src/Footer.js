9ca3ae2ba96c21b7cd472098ceade5391ce10fee
kimndex <122868975+kimndex@users.noreply.github.com>
2023-09-08 08:19:51 +0100

    updated source-code

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,8 @@ import "./footer.css";
 export default function Footer() {
   return (
     <p id="footer" className="mt-5">
-      <a href="https://github.com/kimndex/SheCodes-Plus-Week-5">
-        open-source code
-      </a>
-      , by Nothy Ndebele
+      <a href="https://github.com/kimndex/my-app">open-source code</a>, by Nothy
+      Ndebele
     </p>
   );
 }
