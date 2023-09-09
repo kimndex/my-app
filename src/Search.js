c387f5d86c544220eb9efab02e8dcb0030bc44c4
kimndex <122868975+kimndex@users.noreply.github.com>
2023-09-09 15:49:51 +0100

    changed link from http to https

--- src/Search.js
+++ src/Search.js
@@ -18,7 +18,7 @@ export default function Search(props) {
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       desc: response.data.weather[0].description,
       city: response.data.name,
     });
